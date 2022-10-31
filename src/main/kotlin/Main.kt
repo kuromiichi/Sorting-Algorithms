4a01f003971abaf97735ffe51f6c8601a6e457ed
Alex Fernández <afbarranco@gmail.com>
2022-10-31 13:21:28 +0100

    Main preparado para ejecutar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,16 @@
 fun main() {
-    val mult = 1000
-    for (i in 10..1000 step 10) {
-        println("${i * mult} elements: ${getSortingTime(5, i * mult, "shell") / 1000_000} ms")
+    // Test for 200 results
+    val method = ""
+    val times = LongArray(200)
+    var i = 0
+    for (size in 5000..1000_000 step 5000) {
+        println("Sorting array using $method method (${i + 1}/200)")
+        times[i] = getSortingTime(numberOfArrays = 5, sizeOfArrays = size, method = method)
+        i++
+    }
+    // Print out the results
+    println("Calculation completed")
+    for (elem in times) {
+        println(elem)
     }
 }
